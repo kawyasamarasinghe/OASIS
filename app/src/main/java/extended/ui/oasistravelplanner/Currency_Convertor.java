848bbba3754b0bbed374a939bf7ac329b200d188
BawanthiGunasekara <bawanthik99@gmail.com>
2022-05-08 15:28:59 +0530

    Implemented the Currency Convertor

--- app/src/main/java/extended/ui/oasistravelplanner/Currency_Convertor.java
+++ app/src/main/java/extended/ui/oasistravelplanner/Currency_Convertor.java
@@ -3,12 +3,61 @@ package extended.ui.oasistravelplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Currency_Convertor extends AppCompatActivity {
+
+    Spinner from_currency, to_currency;
+    EditText currency, result;
+    Button convertBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currency_convertor);
+
+        currency = (EditText) findViewById(R.id.textxField1);
+        from_currency = (Spinner) findViewById(R.id.from_currency);
+        to_currency = (Spinner) findViewById(R.id.to_currency);
+        convertBtn = (Button) findViewById(R.id.convert);
+        result = (EditText) findViewById(R.id.result);
+
+
+        String[] from = {"USD"};
+        ArrayAdapter ad = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item,from);
+        from_currency.setAdapter(ad);
+
+        String[] to = {"Indian Rupees","Sri Lankan Rupees"};
+        ArrayAdapter ad1 = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item,to);
+        to_currency.setAdapter(ad1);
+
+        convertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public  void  onClick(View v){
+                Double tot;
+                Double amount = Double.parseDouble(currency.getText().toString());
+                if(from_currency.getSelectedItem().toString() .equals("USD" ) && to_currency.getSelectedItem().toString().equals( "Indian Rupees"))
+                {
+                    tot = amount * 70.0;
+                    result.setText(String.format("Answer: %.2f", tot));
+                    Toast.makeText(getApplicationContext(),tot.toString(),Toast.LENGTH_LONG).show();
+                }
+
+                else if(from_currency.getSelectedItem().toString() .equals("USD")  && to_currency.getSelectedItem().toString().equals("Sri Lankan Rupees"))
+                {
+                    tot = amount * 180.0;
+                    result.setText(String.format("Answer: %.2f", tot));
+                    Toast.makeText(getApplicationContext(),tot.toString(),Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+        });
+
     }
 }
--- app/src/main/java/extended/ui/oasistravelplanner/model/MemoriesModel.java
+++ app/src/main/java/extended/ui/oasistravelplanner/model/MemoriesModel.java
@@ -1,4 +1,48 @@
 package extended.ui.oasistravelplanner.model;
 
 public class MemoriesModel {
+    String date, title, place, description;
+
+    MemoriesModel(){
+
+    }
+
+    public MemoriesModel(String title, String place, String date, String description){
+        this.title = title;
+        this.place = place;
+        this.date = date;
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
